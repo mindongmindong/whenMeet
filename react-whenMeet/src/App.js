63725e4c4ab87dbc34aaf41cfc49c55b2b453e22
mindongmindong <dcmin123@ajou.ac.kr>
2023-11-12 12:10:30 +0900

    Remove Delete file & modified files

--- react-whenMeet/src/App.js
+++ react-whenMeet/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState,useEffect} from "react";
+import HomeMake from "./routes/HomeMake"
+import MeetingInfo from "./routes/MeetingInfo"
+import LinkPage from "./routes/LinkPage"
+import HomeParticipate from "./routes/HomeParticipate"
+import UserTimeInfo from "./routes/HomeMake"
+import{
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomeMake/>}> </Route>
+        <Route path="/homeparticipate" element={<HomeParticipate />}></Route>
+        <Route path="/meetinginfo" element={<MeetingInfo />}> </Route>
+        <Route path="/meetinginfo/linkpage" element={<LinkPage />}></Route>
+        <Route path="/homeparticipate/usertimeinfo" element={<UserTimeInfo />}></Route>
+      </Routes>
+    </Router>
   );
 }
+
 
 export default App;
